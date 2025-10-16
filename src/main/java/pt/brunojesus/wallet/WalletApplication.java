2f8126d92b550a3d750b093708b283a19569d8aa
Bruno Jesus <bruno.fl.jesus@gmail.com>
2025-10-17 00:37:39 +0100

    Initial commit

---
+++ src/main/java/pt/brunojesus/wallet/WalletApplication.java
@@ -0,0 +1,15 @@
+package pt.brunojesus.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "pt.brunojesus.wallet.repository")
+public class WalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/brunojesus/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.brunojesus.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
